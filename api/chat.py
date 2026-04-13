d9f71ec2cd08925293b9c6dba35141ee421636b4
ivancordones <ivancordonesnunez@gmail.com>
2026-04-13 08:46:41 +0200

    minimize project for vercel build

--- api/chat.py
+++ api/chat.py
@@ -2,7 +2,6 @@ from http.server import BaseHTTPRequestHandler
 import json
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
