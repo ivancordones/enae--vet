4c2983bc291aad3e9aa5a4b8a98625ef56773722
ivancordones <ivancordonesnunez@gmail.com>
2026-04-13 08:44:50 +0200

    fix: missing BaseHTTPRequestHandler import

--- api/chat.py
+++ api/chat.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler
 import json
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
